d05f999c4722d085c9c8717146bbf96f3e1e661a
Naveen Rao <naveenkumar.rao@gmail.com>
2015-01-26 02:07:31 +0530

    First commit of the working code for run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,52 @@
+#1.Merges the training and the test sets to create one data set.
+
+features<- read.table("features.txt")
+
+xtest<- read.table("./test/X_test.txt", header=FALSE)
+xtrain<- read.table("./train/X_train.txt", header=FALSE)
+colnames(xtest)<- features[,2]
+colnames(xtrain)<- features[,2]
+
+ytest<- read.table("./test/Y_test.txt", header=FALSE)
+ytrain<- read.table("./train/Y_train.txt", header=FALSE)
+colnames(ytest)<- "activity_id"
+colnames(ytrain)<- "activity_id"
+
+stest<- read.table("./test/subject_test.txt", header=FALSE)
+strain<- read.table("./train/subject_train.txt", header=FALSE)
+colnames(stest)<- "subject_id"
+colnames(strain)<- "subject_id"
+
+xtest<- cbind(xtest, stest)
+xtrain<- cbind(xtrain, strain)
+
+xytest<- cbind(xtest, ytest)
+xytrain<- cbind(xtrain, ytrain)
+
+df<- rbind(xytest,xytrain)
+
+#2.Extracts only the measurements on the mean and standard deviation for each measurement
+rexp_pattern<- "(mean\\(\\)|std\\(\\))"
+subset_match<- grepl(rexp_pattern, features[,2])
+df_subset<- df[ ,subset_match]
+
+#3.Uses descriptive activity names to name the activities in the data set
+activity_labels<- read.table("activity_labels.txt")
+colnames(activity_labels) <- c("activity_id", "activity")
+full_df<- merge(df_subset, activity_labels, by.x="activity_id", by.y="activity_id")
+
+#4.Appropriately labels the data set with descriptive variable names. 
+names(full_df) <- gsub("[)(-]", "", names(full_df))
+names(full_df) <- gsub('std',"StandardDeviation", names(full_df))
+names(full_df) <- gsub('mean',"Mean", names(full_df))
+names(full_df) <- gsub('\\,',"", names(full_df))
+names(full_df) <- gsub('gravity',"Gravity", names(full_df))
+names(full_df) <- gsub('BodyBody',"Body", names(full_df))
+
+#5.creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+tidydataset_df <- aggregate(full_df[ ,2:67]
+                            , list(subject_id = full_df$subject_id
+                            , activity = full_df$activity)
+                            , mean) 
+write.table(file="tidy.txt", tidydataset_df, row.name=FALSE)
---
+++ specdata/complete.R
@@ -0,0 +1,22 @@
+complete<- function(directory, id=1:332){
+data1<-data.frame(id=numeric(),nobs=numeric())
+cnt<-0
+cnt<-id
+dir<- "C:/Users/innarao/Documents/Coursera"
+#directory<- "specdata"
+mydir<- paste(dir,"/",directory,sep="")
+setwd(mydir)
+filenames<- list.files(mydir)
+
+for (i in cnt)
+  {
+  df<- read.csv(filenames[i], header=T)  
+  x<- df[complete.cases(df),]
+  #print(x)
+  count1<- nrow(x)
+  newrow<-data.frame(id=i,nobs=count1)
+  data1<-rbind(data1,newrow)
+  }
+return(data1)
+
+}
